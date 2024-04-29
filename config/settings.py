82b4ca4b24613b853674ca1264a1c59e026eccb9
Smetankin <smet-eb@yandex.ru>
2024-04-29 16:23:41 +0300

    in progress

--- config/settings.py
+++ config/settings.py
@@ -189,3 +189,9 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': timezone.timedelta(days=1),
     },
 }
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
